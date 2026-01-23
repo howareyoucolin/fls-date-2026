74024dc67efba98e2e7ad19dbfe29f5d82838c45
Colin Zhao <colin@townsquaredigital.com>
2026-01-23 17:00:54 -0500

    remove public_old folder

--- public_old/actions/login.php
+++
@@ -1,5 +0,0 @@
-<?php if( ! defined('ROOT_PATH') ) die( 'Curiosity kills cat!' );
-
-// Redirect to home page
-header( 'Location: ' . SITE_URL );
-exit(0);
--- public_old/constants.php
+++
@@ -1,3 +0,0 @@
-<?php if( ! defined('ROOT_PATH') ) die( 'Curiosity kills cat!' );
-
-define( 'DEFAULT_SILHOUETTE', '//www.flushingdating.com/assets/images/nobody.png' );
