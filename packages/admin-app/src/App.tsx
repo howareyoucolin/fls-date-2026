4612521cd00c665f4a1e45b440e1688b0321f7f8
Colin Zhao <colin@townsquaredigital.com>
2026-01-27 01:17:29 -0500

    update admin app title

--- packages/admin-app/src/App.tsx
+++ packages/admin-app/src/App.tsx
@@ -1,55 +1,55 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { SignedIn, SignedOut } from "@clerk/clerk-react";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import ClerkLogin from "./pages/ClerkLogin";
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { SignedIn, SignedOut } from '@clerk/clerk-react'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import ClerkLogin from './pages/ClerkLogin'
 
 export default function App() {
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <>
-            <SignedIn>
-              <Home />
-            </SignedIn>
-            <SignedOut>
-              <Navigate to="/login" replace />
-            </SignedOut>
-          </>
-        }
-      />
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <>
+                        <SignedIn>
+                            <Home />
+                        </SignedIn>
+                        <SignedOut>
+                            <Navigate to="/login" replace />
+                        </SignedOut>
+                    </>
+                }
+            />
 
-      {/* Landing */}
-      <Route
-        path="/login"
-        element={
-          <>
-            <SignedOut>
-              <Login />
-            </SignedOut>
-            <SignedIn>
-              <Navigate to="/" replace />
-            </SignedIn>
-          </>
-        }
-      />
+            {/* Landing */}
+            <Route
+                path="/login"
+                element={
+                    <>
+                        <SignedOut>
+                            <Login />
+                        </SignedOut>
+                        <SignedIn>
+                            <Navigate to="/" replace />
+                        </SignedIn>
+                    </>
+                }
+            />
 
-      {/* Actual Clerk sign-in */}
-      <Route
-        path="/login/clerk"
-        element={
-          <>
-            <SignedOut>
-              <ClerkLogin />
-            </SignedOut>
-            <SignedIn>
-              <Navigate to="/" replace />
-            </SignedIn>
-          </>
-        }
-      />
-    </Routes>
-  );
+            {/* Actual Clerk sign-in */}
+            <Route
+                path="/login/clerk"
+                element={
+                    <>
+                        <SignedOut>
+                            <ClerkLogin />
+                        </SignedOut>
+                        <SignedIn>
+                            <Navigate to="/" replace />
+                        </SignedIn>
+                    </>
+                }
+            />
+        </Routes>
+    )
 }
